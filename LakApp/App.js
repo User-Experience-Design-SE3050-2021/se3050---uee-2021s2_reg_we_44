f766a00a5e3f85825cb6ef9966ddd273627cd78a
sachcha99 <sachcha99@gmail.com>
2021-09-19 17:17:34 +0530

    CompanyAddForm Added

--- LakApp/App.js
+++ LakApp/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabScreen from "./src/screens/Tab";
 import PostAdCategoryScreen from "./src/screens/ads/PostAdCategory";
+import CompanyAddForm from "./src/screens/Company/CompanyAddForm"
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,26 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={"Home"}>
         <Stack.Screen name="Home" component={TabScreen}></Stack.Screen>
+        <Stack.Screen
+          options={{
+            title: 'vdvd',
+            headerStyle: {
+              backgroundColor: '#5956E9',
+              elevation: 0,
+              shadowOpacity: 0,
+
+            },
+            headerTintColor: '#000',
+            headerTitleStyle: {
+              fontWeight: '700',
+              fontSize: 20,
+              headerShadowVisible: false,
+              textAlign: "left",
+              fontFamily: "Raleway-Bold"
+            },
+          }}
+          name="CompanyAddForm" component={CompanyAddForm} />
+
       </Stack.Navigator>
     </NavigationContainer>
   )
